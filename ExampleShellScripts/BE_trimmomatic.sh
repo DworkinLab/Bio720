54945d60dad8a3d74c724313ec9c7f1e4953474c
Ian Dworkin <idworkin@msu.edu>
2015-10-06 22:12:17 -0400

    added \ to BE script for clarity

--- ExampleShellScripts/BE_trimmomatic.sh
+++ ExampleShellScripts/BE_trimmomatic.sh
@@ -9,5 +9,6 @@ Ovary”
 
 for file in $files
 do
-	java -jar /work/ben/Trimmomatic-0.32/trimmomatic-0.32.jar PE -phred33 -trimlog ${file}_log.txt AO248_${file}_R1.fastq.gz AO248_${file}_R2.fastq.gz AO248_${file}_R1_trim_paired.fastq.gz AO248_${file}_R1_trim_single.fastq.gz AO248_${file}_R2_trim_paired.fastq.gz AO248_${file}_R2_trim_single.fastq.gz ILLUMINACLIP:/work/ben/Trimmomatic-0.32/adapters/trimmomatic_adapters.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:36
+	java -jar /work/ben/Trimmomatic-0.32/trimmomatic-0.32.jar PE -phred33 -trimlog \
+	${file}_log.txt AO248_${file}_R1.fastq.gz AO248_${file}_R2.fastq.gz AO248_${file}_R1_trim_paired.fastq.gz \ AO248_${file}_R1_trim_single.fastq.gz AO248_${file}_R2_trim_paired.fastq.gz AO248_${file}_R2_trim_single.fastq.gz \ ILLUMINACLIP:/work/ben/Trimmomatic-0.32/adapters/trimmomatic_adapters.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:36
 done
